f94a33138fdd1b7d4ff37d6302bb0d259f8b4ff1
Bryce St. Pierre <brycelstpierre@gmail.com>
2018-09-01 19:28:04 -0400

    Updated main script and package.json.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+#!/usr/bin/env node
+
 // https://nodejs.org/docs/latest/api/process.html#process_process_argv
+
+// Delete the 0 and 1 argument (node and script.js)
+var args = process.argv.splice(process.execArgv.length + 2);
+
+
 
 process.argv.forEach((val, index) => {
   console.log(`${index}: ${val}`);
 });
-
-console.log(process.argv0);
