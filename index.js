a4b6594dce88350dccce0c645b3fbafc650059d0
Bryce St. Pierre <brycelstpierre@gmail.com>
2018-09-10 19:04:24 -0400

    Updated index.js.

--- index.js
+++ index.js
@@ -4,41 +4,55 @@ const commandLineUsage = require('command-line-usage');
 
 
 const definitions = [
-  { name: 'help', alias: 'h', type: Boolean, description: 'Print the help guide.' }
+  { 
+    name: 'country', 
+    alias: 'c', 
+    type: String, 
+    description: 'Specify the country to search.' 
+  }, { 
+    name: 'city', 
+    alias: 'n', 
+    type: String, 
+    description: 'Specify the city to search.' 
+  }, {
+    name: 'latlng',
+    alias: 'l',
+    type: Number,
+    multiple: true,
+    description: 'Specify the latitude/longitude coordinates to search.'
+  }, { 
+    name: 'output', 
+    alias: 'o', 
+    type: String, 
+    description: 'Specify the output filename.' 
+  }, { 
+    name: 'help', 
+    alias: 'h', 
+    type: Boolean, 
+    description: 'Print the help guide.' 
+  }
 ];
-// const optionDefinitions = [
-//   { name: 'verbose', alias: 'v', type: Boolean },
-//   { name: 'src', type: String, multiple: true, defaultOption: true },
-//   { name: 'timeout', alias: 't', type: Number }
-// ]
 
 const options = commandLineArgs(definitions);
 
-const sections = [
+const guide = commandLineUsage([
   {
     header: 'Climate Normals CLI',
     content: 'A simple utility for downloading climate normal data for Canada and the United States.'
   },
   {
-    header: 'Synopsis',
-    content: '$ climate-normals <options> <command>'
+    header: 'Sample Commands',
+    content: '> climate-normals <options>\n> climate-normals -c canada --city toronto\n> climate-normals --country us -n "los angeles"\n> climate-normals -c us -n chicago -o chicago'
   },
   {
     header: 'Options',
     optionList: definitions
-    // optionList: [
-    //   {
-    //     name: 'output',
-    //     typeLabel: '{underline filename}',
-    //     description: 'The input to process.'
-    //   },
-    //   {
-    //     name: 'help',
-    //     description: 'Print this usage guide.'
-    //   }
-    // ]
   }
-]
-const usage = commandLineUsage(sections);
+]);
+
+process.stdout.write(JSON.stringify(options));
+// console.log(options);
+
+
 if (options.help)
-  console.log(usage);
+  console.log(guide);
